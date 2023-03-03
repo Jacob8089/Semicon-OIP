c2b4e964de9aa674cbfb3ac866be2b4eab1204cb
Jacob <thomas.jacob201@gmail.com>
2023-03-04 00:10:38 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+a=[5,10,6,4,2,7,3,4,1]
+
+def mean():
+    sum=0
+
+    for i in range(0,len(a)):
+        sum+=a[i]
+    mean_val=sum/len(a)
+    return round(mean_val,4)
+
+def variance():
+    var=0
+    for i in range(0,len(a)):
+         var+=(a[i]-mean())**2
+    var_val=var/len(a)
+    return round(var_val,4)
+
+def sv():
+    sv_val=variance()**0.5
+    return round(sv_val,4)
+
+print(mean())
+print(variance())
+print(sv())
